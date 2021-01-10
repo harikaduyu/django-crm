8139264a27935da7dc83c8973891e3215143f0b7
Harika <h.duyu@rebuy.com>
2021-01-10 08:10:30 +0100

    add foreign key relations

--- accounts/models.py
+++ accounts/models.py
@@ -30,8 +30,8 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivered', 'Delivered'),
     )
-    # customer =
-    # product =
-
+    customer = models.ForeignKey(
+        Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
