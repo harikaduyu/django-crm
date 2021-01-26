64308aacbcf7c7ef84c07a053826b3b7a3a2dd73
Harika <h.duyu@rebuy.com>
2021-01-26 08:00:45 +0100

    hiding secrets

--- crm/settings.py
+++ crm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b6ck5kz4+4-$83ekjv4zda5rwnn_%ar9az-!=bmhdwq^^fc-&*'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
