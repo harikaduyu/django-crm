07f79be00b84c68b0042b2270da6555dd4f32ad6
Harika <h.duyu@rebuy.com>
2021-01-29 06:55:20 +0100

    add built in django password reset views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -13,4 +14,12 @@ urlpatterns = [
     path('create_order/<str:pk>/', views.create_order_view, name='create_order'),
     path('update_order/<str:pk>/', views.update_order_view, name='update_order'),
     path('delete_order/<str:pk>/', views.delete_order_view, name='delete_order'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(),
+         name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),
+         name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),
+         name="password_reset_complete")
 ]
