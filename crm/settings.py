e80040bf7488c513f76be8fc1c3622ef91a5bb2f
Harika <h.duyu@rebuy.com>
2021-01-29 06:56:40 +0100

    configure smt for gmail

--- crm/settings.py
+++ crm/settings.py
@@ -129,3 +129,12 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / "static/images"
+
+
+# SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# email address and pw are hidden in the secrets.py file
